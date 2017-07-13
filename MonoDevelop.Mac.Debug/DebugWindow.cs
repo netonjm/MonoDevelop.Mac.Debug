10243d6eec23150389eecfb9e0bbfcdc78fd4d65
Jose Medrano <jose.medrano@microsoft.com>
2017-07-13 21:16:17 +0200

    Some minor changes

--- MonoDevelop.Mac.Debug/DebugWindow.cs
+++ MonoDevelop.Mac.Debug/DebugWindow.cs
@@ -9,6 +9,7 @@ namespace MonoDevelop.Mac.Debug
 	public class DebugWindow : NSWindow
 	{
 		NSMenu menu;
+		NSView view, nextKeyView, previousKeyView;
 
 		BorderedWindow debugOverlayWindow;
 		BorderedWindow debugNextOverlayWindow;
@@ -20,48 +21,105 @@ namespace MonoDevelop.Mac.Debug
 
 		bool IsNextResponderOverlayVisible {
 			get {
-				return debugNextOverlayWindow?.Visible ?? false;
+				return debugNextOverlayWindow.Visible;
 			}
 			set {
-				if (debugNextOverlayWindow == null)
-					return;
 				debugNextOverlayWindow.Visible = value;
 			}
 		}
 
 		bool IsPreviousResponderOverlayVisible {
 			get {
-				return debugPreviousOverlayWindow?.Visible ?? false;
+				return debugPreviousOverlayWindow.Visible;
 			}
 			set {
-				if (debugPreviousOverlayWindow == null)
-					return;
 				debugPreviousOverlayWindow.Visible = value;
 			}
 		}
 
 		bool IsFirstResponderOverlayVisible {
 			get {
-				return debugOverlayWindow?.Visible ?? false;
+				return debugOverlayWindow.Visible;
 			}
 			set {
-				if (debugOverlayWindow == null)
-					return;
 				debugOverlayWindow.Visible = value;
 			}
 		}
 
+		bool IsStatusWindowVisible {
+			get {
+				return debugStatusWindow.ParentWindow != null;
+			}
+			set {
+				ShowStatusWindow (value);
+			}
+		}
+
+		#endregion
+
 		public DebugWindow (IntPtr handle) : base (handle)
 		{
+			if (debugOverlayWindow == null) {
+				debugOverlayWindow = new BorderedWindow (CGRect.Empty, NSColor.Blue);
+				AddChildWindow (debugOverlayWindow, NSWindowOrderingMode.Above);
+			}
+			if (debugNextOverlayWindow == null) {
+				debugNextOverlayWindow = new BorderedWindow (CGRect.Empty, NSColor.Yellow);
+				AddChildWindow (debugNextOverlayWindow, NSWindowOrderingMode.Above);
+			}
 
+			if (debugPreviousOverlayWindow == null) {
+				debugPreviousOverlayWindow = new BorderedWindow (CGRect.Empty, NSColor.Red);
+				AddChildWindow (debugPreviousOverlayWindow, NSWindowOrderingMode.Above);
+			}
+
+			if (debugStatusWindow == null) {
+				debugStatusWindow = new StatusWindow (new CGRect (10, 10, 300, 500));
+			}
 		}
+
+		void ShowStatusWindow (bool value)
+		{
+			if (value) {
+				if (!IsStatusWindowVisible) {
+					AddChildWindow (debugStatusWindow, NSWindowOrderingMode.Above);
+					RefreshStatusWindow ();
+				}
+			}
+			else {
+				debugStatusWindow?.Close ();
+			}
+		}
+
+		public void RefreshStatusWindow ()
+		{
+			debugStatusWindow.AlignWith (Frame);
+
+			var anyFocusedView = view != null;
+			if (!anyFocusedView)
+				return;
+
+			var elements = view.GenerateLog ("Current");
+			if (nextKeyView != null) {
+				elements.AddRange (nextKeyView.GenerateLog ("Next"));
+			};
+			if (previousKeyView != null) {
+				elements.AddRange (previousKeyView.GenerateLog ("Previous"));
+			};
+
+			debugStatusWindow.GenerateStatusView (elements);
+		}
+
+		#region Public API
 
 		public void SetMenu (NSMenu menu)
 		{
 			this.menu = menu ?? throw new NullReferenceException ("Menu cannot be null");
 			int menuCount = 0;
 			var submenu = this.menu.ItemAt (0).Submenu;
-
+			submenu.AutoEnablesItems = false;
+			submenu.InsertItem (new NSMenuItem (string.Format ("KeyViewLoop Debugger v{0}",GetAssemblyVersion ()), ShowHideDetailDebuggerWindow) { Enabled = false }, menuCount++);
+			submenu.InsertItem (NSMenuItem.SeparatorItem, menuCount++);
 			submenu.InsertItem (new NSMenuItem ("Show KeyViewLoop Debug Window", ShowHideDetailDebuggerWindow), menuCount++);
 			submenu.InsertItem (new NSMenuItem ("Show First Responder Overlay", ShowFirstResponderOverlayHandler), menuCount++);
 			submenu.InsertItem (new NSMenuItem ("Show Next Responder Overlay", ShowNextResponderOverlayHandler), menuCount++);
@@ -70,13 +128,15 @@ namespace MonoDevelop.Mac.Debug
 			submenu.InsertItem (NSMenuItem.SeparatorItem, menuCount++);
 		}
 
+		#endregion
+
 		void ShowFirstResponderOverlayHandler (object sender, EventArgs e)
 		{
 			IsFirstResponderOverlayVisible = !IsFirstResponderOverlayVisible;
 			RefreshDebugData (FirstResponder);
 
 			var menuItem = (NSMenuItem)sender;
-			menuItem.Title = string.Format ("{0} First Responder Overlay", ToMenuAction (IsFirstResponderOverlayVisible));
+			menuItem.Title = string.Format ("{0} First Responder Overlay", ToMenuAction (!IsFirstResponderOverlayVisible));
 		}
 
 		void ShowPreviousResponderOverlayHandler (object sender, EventArgs e)
@@ -85,7 +145,7 @@ namespace MonoDevelop.Mac.Debug
 			RefreshDebugData (FirstResponder);
 
 			var menuItem = (NSMenuItem)sender;
-			menuItem.Title = string.Format ("{0} Previous Responder Overlay", ToMenuAction (IsPreviousResponderOverlayVisible));
+			menuItem.Title = string.Format ("{0} Previous Responder Overlay", ToMenuAction (!IsPreviousResponderOverlayVisible));
 		}
 
 		void ShowNextResponderOverlayHandler (object sender, EventArgs e)
@@ -94,12 +154,15 @@ namespace MonoDevelop.Mac.Debug
 			RefreshDebugData (FirstResponder);
 
 			var menuItem = (NSMenuItem)sender;
-			menuItem.Title = string.Format ("{0} Next Responder Overlay", ToMenuAction (IsNextResponderOverlayVisible));
+			menuItem.Title = string.Format ("{0} Next Responder Overlay", ToMenuAction (!IsNextResponderOverlayVisible));
 		}
 
 		void ShowHideDetailDebuggerWindow (object sender, EventArgs e)
 		{
-			debugStatusWindow.IsVisible = !debugStatusWindow.IsVisible;
+			IsStatusWindowVisible = !IsStatusWindowVisible;
+
+			var menuItem = (NSMenuItem)sender;
+			menuItem.Title = string.Format ("{0} KeyViewLoop Debug Window", ToMenuAction (!IsStatusWindowVisible));
 		}
 
 		string ToMenuAction (bool value)
@@ -109,51 +172,22 @@ namespace MonoDevelop.Mac.Debug
 
 		void RefreshDebugData (NSResponder firstResponder)
 		{
-			var view = firstResponder as NSView;
+			view = firstResponder as NSView;
 			if (view != null) {
-
-				if (debugOverlayWindow == null) {
-					debugOverlayWindow = new BorderedWindow (CGRect.Empty, NSColor.Blue);
-					AddChildWindow (debugOverlayWindow, NSWindowOrderingMode.Above);
-				}
-
 				debugOverlayWindow.AlignWith (view);
-
-				var nextKeyView = view.NextValidKeyView as NSView;
-				if (nextKeyView != null) {
-					if (debugNextOverlayWindow == null) {
-						debugNextOverlayWindow = new BorderedWindow (CGRect.Empty, NSColor.Yellow);
-						AddChildWindow (debugNextOverlayWindow, NSWindowOrderingMode.Above);
-					}
-					debugNextOverlayWindow.AlignWith (nextKeyView);
-				}
-
-				var previousKeyView = view.PreviousValidKeyView as NSView;
-				if (previousKeyView != null) {
-					if (debugPreviousOverlayWindow == null) {
-						debugPreviousOverlayWindow = new BorderedWindow (CGRect.Empty, NSColor.Red);
-						AddChildWindow (debugPreviousOverlayWindow, NSWindowOrderingMode.Above);
-					}
-					debugPreviousOverlayWindow.AlignWith (previousKeyView);
-				}
-
-				if (debugStatusWindow == null) {
-					debugStatusWindow = new StatusWindow (new CGRect (10, 10, 300, 500));
-					AddChildWindow (debugStatusWindow, NSWindowOrderingMode.Above);
-				}
-
-				debugStatusWindow.AlignWith (Frame);
-
-				var elements = view.GenerateLog ("Current");
-				if (nextKeyView != null) {
-					elements.AddRange (nextKeyView.GenerateLog ("Next"));
-				};
-				if (previousKeyView != null) {
-					elements.AddRange (previousKeyView.GenerateLog ("Previous"));
-				};
-
-				debugStatusWindow.GenerateStatusView (elements);
 			}
+
+			nextKeyView = view?.NextValidKeyView as NSView;
+			if (nextKeyView != null) {
+				debugNextOverlayWindow.AlignWith (nextKeyView);
+			}
+
+			previousKeyView = view?.PreviousValidKeyView as NSView;
+			if (previousKeyView != null) {
+				debugPreviousOverlayWindow.AlignWith (previousKeyView);
+			}
+
+			RefreshStatusWindow ();
 		}
 
 		public override void BecomeMainWindow ()
@@ -165,6 +199,13 @@ namespace MonoDevelop.Mac.Debug
 		{
 			RefreshDebugData (aResponder);
 			return base.MakeFirstResponder (aResponder);
+		}
+
+		string GetAssemblyVersion ()
+		{
+			var assembly = System.Reflection.Assembly.GetExecutingAssembly ();
+			var fileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo (assembly.Location);
+			return fileVersionInfo.ProductVersion;
 		}
 	}
 }
