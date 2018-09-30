a4a942d0ac4a42ae60407d4a6871eee38b68486e
José Medrano <jose.medrano@xamarin.com>
2018-09-30 05:40:14 +0200

    Fixes rendering of node items

--- MonoDevelop.Mac.Debug/OutlineView.cs
+++ MonoDevelop.Mac.Debug/OutlineView.cs
@@ -58,17 +58,10 @@ namespace MonoDevelop.Mac.Debug
 			const string identifer = "myCellIdentifier";
 			public override NSView GetView (NSOutlineView outlineView, NSTableColumn tableColumn, NSObject item)
 			{
-				NSTextField view = (NSTextField)outlineView.MakeView (identifer, this);
+				var view = (NSTextField)outlineView.MakeView (identifer, this);
 				if (view == null) {
-					view = new NSTextField () {
-						Identifier = identifer,
-						Bordered = false,
-						Selectable = false,
-						Editable = false
-					};
+					view = NativeViewHelpers.CreateLabel (((Node)item).Name);
 				}
-
-				view.StringValue = ((Node)item).Name;
 				return view;
 			}
 
