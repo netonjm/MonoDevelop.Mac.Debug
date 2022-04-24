c6b073b0db75d5de76ded82423b8130cc517cf45
Jose Medrano <josmed@microsoft.com>
2022-04-24 21:08:11 +0200

    bumps extension

--- VisualStudio.ViewInspector.Extension/Properties/AddinInfo.cs
+++ VisualStudio.ViewInspector.Extension/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly: Addin(
 	"ViewInspector",
 	Namespace = "VisualStudio",
-	Version = "0.8.3"
+	Version = "0.8.4"
 )]
 
 [assembly: AddinName("VisualStudio View Inspector")]
